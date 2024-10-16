69271f2bc67a9cb08d6af8e394fb2dc651ad0590
Vasyl Pylypchynets <pylypchynets.vasyl@gmail.com>
2024-10-16 12:09:24 +0300

    make build

---
+++ index.da828fad.js
@@ -0,0 +1,2 @@
+!function e(n,t){var r=document.createElement("ul");Object.keys(t).forEach(function(n){var a=document.createElement("li");a.textContent=n,Object.keys(t[n]).length>0&&e(a,t[n]),r.appendChild(a)}),n.appendChild(r)}(document.querySelector("#tree"),{Drink:{Wine:{},Schnaps:{}},Fruit:{Red:{Cherry:{},Strawberry:{}},Yellow:{Banana:{},Pineapple:{}}}});
+//# sourceMappingURL=index.da828fad.js.map
